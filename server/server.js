c004fc0d35a50a477cd56cf8fac6914c4005c62b
SSaquif <ssaquif@gmail.com>
2020-05-03 16:01:30 -0400

    react set up

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ const socketIO = require('socket.io');
 const morgan = require('morgan');
 
 const PORT = 8080;
+
+//to server files from ../client folder instead of react
 const staticPath = path.join(`${__dirname}`, '..', 'client');
 console.log(staticPath);
+//----------------------------------------------------//
 
 const app = express();
 const server = http.createServer(app); //adding express listener to server
@@ -14,7 +17,8 @@ const io = socketIO(server); //adding socketIO listener to Server
 
 //middle wares
 app.use(morgan('dev'));
-app.use(express.static(staticPath));
+
+//app.use(express.static(staticPath)); //to use ../client instead of react
 
 io.on('connection', (sock) => {
 	console.log('Someone Connected, io works');
