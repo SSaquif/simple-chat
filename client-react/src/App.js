28caadffccddd5209d3bc2a283ad7adb2f34fa86
SSaquif <ssaquif@gmail.com>
2020-05-03 16:21:03 -0400

    set up basic UI

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<Fragment>
+			<div className="log-events-wrapper">
+				<h3>Chat Away</h3>
+				<ul id="log-events"></ul>
+			</div>
+			<div className="message-box-wrapper">
+				<form>
+					<input type="text" id="chat-box" />
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
