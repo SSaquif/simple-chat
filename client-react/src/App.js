556c459e34c442c41bc469a1ff989764ff3689e8
SSaquif <ssaquif@gmail.com>
2020-05-03 22:01:21 -0400

    Basic UI Completed

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,21 +1,99 @@
-import React, { Fragment } from 'react';
-import './App.css';
+import React, { Fragment, useState } from 'react';
+import styled from 'styled-components';
 
 function App() {
+	const [chatMsg, setChatMsg] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log('Event', chatMsg);
+	};
+
 	return (
 		<Fragment>
-			<div className="log-events-wrapper">
-				<h3>Chat Away</h3>
-				<ul id="log-events"></ul>
-			</div>
-			<div className="message-box-wrapper">
-				<form>
-					<input type="text" id="chat-box" />
-					<button type="submit">Send</button>
-				</form>
-			</div>
+			<ChatHeader>Chat Away</ChatHeader>
+			<ChatBodyWrapper>
+				<ChatSidebar>
+					<ChatSideBarHeader>Chats</ChatSideBarHeader>
+					<ChatGroup>Person/Group 1</ChatGroup>
+					<ChatGroup>Person/Group 2</ChatGroup>
+				</ChatSidebar>
+				<ChatWindow>
+					<ChatWindowHeader>Messages</ChatWindowHeader>
+					<ChatMsgWrapper>
+						<ChatMsgLeft>Msg 1</ChatMsgLeft>
+						<ChatMsgRight>Msg 2</ChatMsgRight>
+					</ChatMsgWrapper>
+					<ChatNewMsgForm
+						onSubmit={(event) => {
+							handleSubmit(event);
+						}}
+					>
+						<ChatInput
+							type="text"
+							id="chat-box"
+							value={chatMsg}
+							onChange={(event) => {
+								setChatMsg(event.target.value);
+							}}
+						/>
+						<ChatSubmitButton type="submit">Send</ChatSubmitButton>
+					</ChatNewMsgForm>
+				</ChatWindow>
+			</ChatBodyWrapper>
 		</Fragment>
 	);
 }
 
 export default App;
+
+//--------CSS----------//
+const ChatHeader = styled.h3`
+	text-align: center;
+`;
+const ChatBodyWrapper = styled.div`
+	display: flex;
+	padding: 30px;
+	margin: 20px;
+	border: 1px solid;
+`;
+const ChatSidebar = styled.div`
+	flex: 1;
+	border-right: 1px solid red;
+`;
+const ChatSideBarHeader = styled.h4`
+	text-align: center;
+`;
+const ChatGroup = styled.div`
+	margin-bottom: 20px;
+`;
+const ChatWindow = styled.div`
+	flex: 4;
+	padding-left: 20px;
+`;
+const ChatWindowHeader = styled.h4`
+	text-align: center;
+`;
+const ChatMsgWrapper = styled.div`
+	padding-bottom: 20px;
+`;
+const ChatMsgLeft = styled.div`
+	text-align: left;
+	margin-left: 40px;
+	margin-bottom: 20px;
+`;
+const ChatMsgRight = styled.div`
+	text-align: right;
+	margin-right: 40px;
+	margin-bottom: 20px;
+`;
+
+const ChatNewMsgForm = styled.form`
+	display: flex;
+`;
+
+const ChatInput = styled.input`
+	flex: 1;
+	margin-right: 10px;
+`;
+const ChatSubmitButton = styled.button``;
